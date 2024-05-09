e5e51f4d033fd5cc08f3c21b6eac6e35e7171aab
SHI <851051279@qq.com>
2024-05-09 09:43:18 +0800

    feat: 新增新闻页面

---
+++ src/pages/news/news.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const title = ref('Hello')
+  title.value = '123'
+
+  onLoad(() => {
+    console.log('页面加载完成')
+  })
+
+  const goback = () => {
+    uni.navigateBack()
+  }
+</script>
+
+<template>
+  <view class="content">
+    <view class="text-area">
+      <button type="primary" @click="goback()">返回</button>
+      <text>新闻页面</text>
+    </view>
+  </view>
+</template>
+
+<style lang="scss"></style>
