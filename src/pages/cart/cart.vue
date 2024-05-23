4507428271629bfa52a322757ae949b3cbd4a8db
SHI <851051279@qq.com>
2024-05-23 16:50:59 +0800

    feat: 完善tab栏,

---
+++ src/pages/cart/cart.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <text>购物车</text>
+</template>
+
+<style></style>
---
+++ src/pages/category/category.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <text>分类</text>
+</template>
+
+<style></style>
